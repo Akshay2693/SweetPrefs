49d5390a279ae7561c80822f27353bca5fac62be
defrancesco <s8gidefr@stud.uni-saarldand.de>
2017-11-23 10:17:58 +0100

    Added javadoc.

--- sweetprefs/src/main/java/eu/giovannidefrancesco/easysharedprefslib/ISharedPrefStorage.java
+++ sweetprefs/src/main/java/eu/giovannidefrancesco/easysharedprefslib/ISharedPrefStorage.java
@@ -1,11 +1,21 @@
 package eu.giovannidefrancesco.easysharedprefslib;
 
 /**
- * Created by jibbo on 9/29/17.
+ * This interface allows users of a SharedPreferences to make use of the
+ * synchronous method .commit(), instead of the asynchronous .apply() method.
  */
 // I want this to be public such that tests and third-parties can use or extend it.
 @SuppressWarnings("WeakerAccess")
 public interface ISharedPrefStorage extends IStorage {
+    /**
+     * Same as .store() but synchronous.
+     * @see IStorage
+     */
     void storeImmediate(String key, Object toStore);
+
+    /**
+     * Same as .reset() but synchronous.
+     * @see IStorage
+     */
     void resetImmediate();
 }
--- sweetprefs/src/main/java/eu/giovannidefrancesco/easysharedprefslib/IStorage.java
+++ sweetprefs/src/main/java/eu/giovannidefrancesco/easysharedprefslib/IStorage.java
@@ -3,11 +3,28 @@ package eu.giovannidefrancesco.easysharedprefslib;
 import android.support.annotation.NonNull;
 
 /**
- * Created by jibbo on 9/29/17.
+ * Interface for saving or retrieving object from a storage.
+ * An implementation of a storage could be Database, SharedPreferences or other means.
  */
-
 public interface IStorage {
+    /**
+     * Saves the object into the storage using key as id.
+     * @param key used to identify the object
+     * @param toStore the object which is stored
+     */
     void store(String key, Object toStore);
+
+    /**
+     * Retrieves an object from the storage using key as id.
+     * @param key the id of the object
+     * @param defaultObj it is returned in case the storage doesn't contain an object associated
+     *                   with the id.
+     * @return the wanted object or defaultObj.
+     */
     @NonNull <T> T get(String key, T defaultObj);
+
+    /**
+     * This removes all the entries previously saved in the storage.
+     */
     void reset();
 }
